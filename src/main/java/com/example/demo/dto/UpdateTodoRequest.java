eacbc07bad8b7777a1f9307edb3ad9bee463a896
seyniss <shepunk2@kbu.ac.kr>
2026-03-11 19:29:34 +0900

    1

---
+++ src/main/java/com/example/demo/dto/UpdateTodoRequest.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateTodoRequest {
+
+    private String title;
+    private String content;
+    private boolean completed;
+
+}
